493c9d650eb633dc61354d3fd5f34ddd46508ddf
Joaoviana <joaopadraoviana@gmail.com>
2018-09-09 14:32:01 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,25 @@ app.get('/todos', (req,res) => {
     res.send({todos});
 }, (e)=> {
     res.status(400).send(e);
+  });
+});
+
+//GET /todos/1231234
+//url parameters
+//id variable on the request object
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err)=> {
+    res.status(400).send();
   });
 });
 
