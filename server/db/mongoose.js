5e2edca30efae35344b6f86bb0480bfa0ad76c96
Joaoviana <joaopadraoviana@gmail.com>
2018-09-10 19:59:45 +0200

    Add tests for GET /users/me and POST /users

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
+
+
+
 
 module.exports = {
   mongoose
